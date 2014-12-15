b62db5aabf8abbf5e3908f5340bb9e910e870602
sacha <sacha.okonowski@gmail.com>
2014-12-15 20:55:55 +0100

    Create CsvReader.class.rb

---
+++ stock/CsvReader.class.rb
@@ -0,0 +1,23 @@
+require 'CSV'
+require './BookInStock.class'
+class CsvReader
+  def initialize
+    #...
+  end
+  def readCSVfile(CSVFile)
+
+    
+
+    end
+
+  end
+  def countItemInStock
+    #...
+  end
+  def amountOfTotalStock
+    #...
+  end
+  def numberOfEachItemInStock
+    #...
+  end
+end
