addca9336712e0c065961bb3390933f3a78df8aa
sacha <sacha.okonowski@gmail.com>
2014-12-15 20:37:06 +0100

    Create start.rb

---
+++ start.rb
@@ -0,0 +1,61 @@
+# puts "hello world"
+# puts "its is now #{Time.now}"
+
+# skyfall= Song.new
+
+
+# def : définir methode
+# 	def somme (m1, m2)
+
+# 	result = m1+m2
+# 	return result
+
+# end
+
+
+def sum(n1, n2)
+	result = n1 + n2
+	return result
+end
+
+def divi(n1, n2)
+	result= n1 / n2
+	return result
+
+end
+
+def multi(n1, n2)
+	result= n1 * n2
+	return result
+
+end
+
+def sous(n1, n2)
+	result= n1 - n2
+	return result
+
+end
+
+
+def mod(n1, n2)
+	result= n1 % n2
+	return result
+
+end
+
+def noel
+	if condition
+
+end
+
+
+
+
+
+
+puts sum(2, 4)
+puts divi(10, 2)
+puts multi(2, 50)
+puts sous(100, 19)
+puts mod(7, 9)
+# puts sum("hercule ", "carotte")
